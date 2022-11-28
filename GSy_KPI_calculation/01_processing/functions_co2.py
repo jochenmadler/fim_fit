8e22f3285cd25fb404d2fda5cd6d48f81702d2f5
jochenmadler <58168497+jochenmadler@users.noreply.github.com>
2022-11-28 14:25:37 +0100

    functions_co2.py: new version, considering higher-level p2p trading

--- GSy_KPI_calculation/01_processing/functions_co2.py
+++ GSy_KPI_calculation/01_processing/functions_co2.py
@@ -18,16 +18,25 @@ def get_datetime_index(uc_dir, nr_months):
         
     return sorted(dt_index)
 
-def get_files_for_uc(uc_nr, uc_dir, nr_months):
+def get_files_for_uc(uc_nr, uc_dir, nr_months, level):
+    if level not in ['region', 'ec', 'house']:
+        raise Exception(f'ERROR: If {level} is not None, it must be in [region, ec, house].')
     fpaths = []
-    # collect bottom-level entities' (house/wind/industry) filepaths
     for root, dir, files in os.walk(top = uc_dir, topdown=True):
         if uc_nr in ['0','1','5']:
             fpaths += [os.path.join(root,f) for f in files if 'member-' in f if '-trades.csv' in f]
         else:
-            fpaths += [os.path.join(root,f) for f in files if any(f'region-{reg}-ec{ec}-trades.csv' in f for reg in range(1,7) for ec in range(6))]
+            if level == 'region':
+                # level == 'region': Collect germany (grid) filepaths
+                fpaths += [os.path.join(root,f) for f in files if 'grid-trades.csv' in f]
+            elif level == 'ec':
+                # level == 'ec': Collect regions' filepats
+                fpaths += [os.path.join(root,f) for f in files if any(f'region-{reg}-trades.csv' in f for reg in range(1,7))]
+            else:
+                # level == 'house': collect bottom-level entities' (house/wind/industry) filepaths
+                fpaths += [os.path.join(root,f) for f in files if any(f'region-{reg}-ec{ec}-trades.csv' in f for reg in range(1,7) for ec in range(6))]
     # for each entity, there must be n files (n = nr_months)
-    assert len(fpaths) % nr_months != 0, f'ERROR: {len(fpaths)} filepaths, not divisible by {nr_months} months. Files are missing.'
+    assert len(fpaths) % nr_months == 0, f'ERROR: {len(fpaths)} filepaths, not divisible by {nr_months} months. Files are missing.'
     
     return fpaths
     
@@ -90,12 +99,21 @@ def rename_e_series(e_green_in, e_grey_in, e_co2e, e_share_green, e):
     
     return e_green_in, e_grey_in, e_co2e, e_share_green
     
-def get_childrens_co2(uc_nr, uc_dir, nr_months):
+def get_childrens_co2(uc_nr, uc_dir, nr_months, level = None, df_p_share_renewable = None):
+    if uc_nr in ['0', '1', '4', '5']:
+        level = 'house'
+    else:
+        if uc_nr == '3' and level not in ['house', 'ec']:
+            raise Exception(f'ERROR: For use case {uc_nr}, level is {level}, but must be in [house, ec].')
+        elif uc_nr in ['2', '2-1', '2-2', '6'] and level not in ['house', 'ec', 'region']:
+            raise Exception(f'ERROR: For use case {uc_nr}, level is {level}, but must be in [house, ec, region].')
+        else:
+            pass
     # create empty df with date time index
     df_out = pd.DataFrame(index=get_datetime_index(uc_dir, nr_months))
     df_out_index = df_out.index
     # obtain all relevant -trades.csv files for uc_nr
-    fpaths = get_files_for_uc(uc_nr, uc_dir, nr_months)
+    fpaths = get_files_for_uc(uc_nr, uc_dir, nr_months, level)
     # get lists of all months filepaths for all parents
     parents_paths = [fpaths[i::int(len(fpaths)/nr_months)] for i in range(int(len(fpaths)/nr_months))]
     cs_names = []
@@ -104,12 +122,21 @@ def get_childrens_co2(uc_nr, uc_dir, nr_months):
         df_p = get_combined_df_p(p_paths, uc_nr)
         # get children (cs) and parent (p)
         all_entities = set(df_p.seller) | set(df_p.buyer)
-        cs = set([i for i in df_p.seller if '_house' in i] + [i for i in df_p.buyer if '_house' in i])
+        cs = set([i for i in df_p.seller if level in i] + [i for i in df_p.buyer if level in i])
         p = all_entities - cs
         for c in cs:
             cs_names.append(c)
             # get energy received from other children (green) and from parent (grey)
             c_green_in, c_grey_in = get_c_trades_dfs({c}, p, cs, df_p, df_out_index)
+            # adjustments based on uc_nr and level: Consider that energy from parent contains green_energy, too
+            if (uc_nr in ['2', '2-1', '2-2', '6'] and level != 'region') or (uc_nr in ['3'] and level != 'ec'):
+                assert df_p_share_renewable is not None, f'use case {uc_nr}, level {level} needs df_p_share_renewable, which is None.'
+                # split parent's energy based on its share_renewable: substract renewable amount from c_grey_in and add it to c_green_in
+                c_green_in_temp = c_green_in + (c_grey_in * df_p_share_renewable[[i for i in df_p_share_renewable.columns if next(iter(p)) in i]].iloc[:,0])
+                #c_green_in_temp = c_green_in + df_p_share_renewable[[i for i in df_p_share_renewable.columns if next(iter(p)) in i]].iloc[:,0] * c_grey_in
+                c_grey_in_temp = c_grey_in - (c_grey_in * df_p_share_renewable[[i for i in df_p_share_renewable.columns if next(iter(p)) in i]].iloc[:,0])
+                #c_grey_in_temp = c_grey_in - (1 - df_p_share_renewable[[i for i in df_p_share_renewable.columns if next(iter(p)) in i]].iloc[:,0]) * c_grey_in
+                c_green_in, c_grey_in = c_green_in_temp, c_grey_in_temp
             # calculate emissions based on factors for green and grey energy [g/kWh]
             c_co2e = co2_footprint(c_green_in, c_grey_in)
             # calculate share renewables, i.e. share green energy [%]
@@ -119,7 +146,7 @@ def get_childrens_co2(uc_nr, uc_dir, nr_months):
             # append to df_out
             df_out = pd.concat([df_out, pd.concat([c_green_in, c_grey_in, c_co2e, c_share_green], axis=1)], axis=1)
 
-    return df_out, cs_names
+    return df_out
         
 def aggregate_entity(df_temp, e_name, level=None):
     if level is None or level not in ['ec', 'region', 'germany']:
@@ -144,18 +171,36 @@ def aggregate_entity(df_temp, e_name, level=None):
     
     return df_temp
 
+def get_bottom_level_df(uc_nr, uc_dir, nr_months):
+    if uc_nr in ['0', '1', '4', '5']:
+        # do not consider parents' share renewables
+        return get_childrens_co2(uc_nr, uc_dir, nr_months)
+    elif uc_nr in ['3']:
+        # calculate ecs' share renewables from regions and consider it when calculating houses' share
+        df_temp = get_childrens_co2(uc_nr, uc_dir, nr_months, level='ec')
+        df_ecs = df_temp[[i for i in df_temp if 'share_renewable' in i]]
+        return get_childrens_co2(uc_nr, uc_dir, nr_months, level='house', df_p_share_renewable=df_ecs)
+    else:
+        # calculate regions' share renewabless from germany/grid and consider it when calulating ecs' share
+        df_temp = get_childrens_co2(uc_nr, uc_dir, nr_months, level='region')
+        df_regs = df_temp[[i for i in df_temp if 'share_renewable' in i]]
+        # consider ecs' share renewable when calculating houses' share
+        df_temp = get_childrens_co2(uc_nr, uc_dir, nr_months, level='ec', df_p_share_renewable=df_regs)
+        df_ecs = df_temp[[i for i in df_temp if 'share_renewable' in i]]
+        return get_childrens_co2(uc_nr, uc_dir, nr_months, level='house', df_p_share_renewable=df_ecs)
 
 def kpi_calculation(home_path, uc_nr):
     nr_months = 4
-    # obtain use case number (uc_nr)
+    # obtain use case number (uc_nr) and check that it is valid
     uc_nr = str(uc_nr)
     assert uc_nr in ['0','1','2','2-1','2-2','3','4','5','6'], f'ERROR: uc_nr is {uc_nr}, must be in [0,1,2,2-1,2-2,3,4,5,6]'
     if uc_nr == '0':
         uc_dir = [i.path for i in os.scandir(home_path) if i.is_dir() if 'base' in i.name.lower() if 'case' in i.name.lower()][0]
     else:
         uc_dir = [i.path for i in os.scandir(home_path) if i.is_dir() if f'case_{uc_nr}' in i.name.lower()][0]
-    # get bottom-level df_out_raw for aggregation (#cols: #entities * 4) and list of all childrens' names (cs_names)
-    df_temp, cs_names = get_childrens_co2(uc_nr, uc_dir, nr_months)
+    # get data frame with all houses' share renewables, which considers higher-levels' share renewables depending on the use case 
+    df_temp = get_bottom_level_df(uc_nr, uc_dir, nr_months)
+    # bottom-up aggregation
     for r in range(1,7):
         # aggregate all houses to ecs
         for e in range(6):
